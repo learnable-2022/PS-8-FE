9ceea06645d5a571b09878dd91142446719cd5d7
Bblissful <blessingedeh121@gmail.com>
2023-05-25 19:04:08 +0100

    made changes to Upload

--- Client/src/Components/Upload.jsx
+++ Client/src/Components/Upload.jsx
@@ -6,9 +6,12 @@ export const Upload = () => {
         <div className="w-[100%] bg-[#e5e5e5]">
         <div className="flex items-center justify-between p-4">
           <h2 className="text-xl font-bold">Payroll</h2>
-          <button className="bg-[#430359] hover:bg-purple-900 text-white font-bold py-2 px-4 rounded">
+         
+          <label htmlFor="payroll" className="cursor-pointer  bg-[#430359] hover:bg-purple-900 text-white font-bold py-3 px-5 rounded-xl">
             Upload
-          </button>
+            
+          </label>
+          <input type="file" className="hidden" id="payroll" name="payroll" />
         </div>
             <div className='p-4'> <div className="bg-[#ededed] px-2 py-20"><p className="flex items-center justify-center text-gray-500 text-sm">No data found</p></div></div> 
       </div>
